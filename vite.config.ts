a957635d1f0bfac21c118287b19168861cf9cf75
Dijana Pavlovic <dijana@edgedb.com>
2025-06-12 13:33:05 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
-  plugins: [react(), mode === "development"].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
