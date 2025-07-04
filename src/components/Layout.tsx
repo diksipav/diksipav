ee6368a62fc1ad130a627ef4f1308b013138ebb4
Dijana Pavlovic <dijana.pavlovic992@gmail.com>
2025-07-04 10:03:57 +0200

    update columns widths

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="px-5 sm:px-11 block sm:grid sm:grid-cols-[1fr_532px_1fr] 2xl:grid-cols-[1fr_612px_1fr] 3xl:grid-cols-[1fr_760px_1fr]">
+    <div className="px-5 sm:px-11 block sm:grid sm:grid-cols-[1fr_532px_1fr] 2xl:grid-cols-[1fr_612px_1fr] 3xl:grid-cols-[1fr_700px_1fr]">
       {children}
     </div>
   );
